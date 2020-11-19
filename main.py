8f952eb1735eb6c1407afa0ec63834009d33e4b5
Art-1313 <gromov.ak@phystech.edu>
2020-11-19 15:47:13 +0300

    slightly fix IO

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame as pg
-from manager import Manager
+import gui
+import solar_IO
+import solar_physics
 
 SCREEN_SIZE = (800, 600)
 
 pg.init()
 
+
+class Manager():
+
+    def __init__(self, screen):
+        self.objects = []
+        self.input_file = 'input.txt'
+        self.output_file = 'output.txt'
+        self.button = gui.Button("start", (100,100), screen, (100, 50))
+        self.slider = gui.Slider((100, 500), [100, 500], screen, (20, 100))
+
+    def get_objects(self):
+
+
+
 screen = pg.display.set_mode(SCREEN_SIZE)
 pg.display.set_caption("Solar system")
 clock = pg.time.Clock()
-
-mgr = Manager()
-
 done = False
 
 while not done:
--- solar_IO.py
+++ solar_IO.py
@@ -1,63 +1,26 @@
-# coding: utf-8
-
-from solar_physics import Object 
-input_filename = 'input_file'
-output_filename = 'statistics'
-
-def read_space_objects_data_from_file(input_file_lines):
-    """Cчитывает данные о космических объектах из файла, создаёт сами объекты
-    Параметры:
-
-    **input_file_lines** — Строки входного файла
-    """
-    
-    objects = []
-    for line in input_file_lines:
-        if len(line.strip()) == 0 or line[0] == '#':
-            continue  # пустые строки и строки-комментарии пропускаем
-        object = Object()
-        parse_object_parameters(line, object)
-        objects.append(object)
+A = []
 
 
-    return objects
-
-def parse_object_parameters(line, obj):
-    """Считывает данные о планете из строки.
-    Предполагается такая строка:
-    Входная строка должна иметь слеюущий формат:
-    X_coord Y_coord Velocity_X Velocity_Y Mass Radius Color
-
-    Параметры:
-
-    **line** — строка с описание объекта.
-    **obj** — объект.
-    """
-    
-    obj.coords[0] = float(line.split()[1])
-    obj.coords[1] = float(line.split()[2])
-    obj.vel[0] = float(line.split()[3])
-    obj.vel[1] = float(line.split()[4])
-    obj.mass = float(line.split()[5])
-    obj.rad = float(line.split()[6])
-    obj.color = line.split()[7]
+def take_info(file_name):
+    global A
+    input = open(file_name, 'r')
+    A = input.readlines()
+    for i in range(len(A)):
+        A[i] = (A[i].split())
+    input.close()
+    return A
 
 
-def write_space_objects_data_to_file(output_filename, space_objects):
-    """Сохраняет данные о космических объектах в файл.
+def save_info(file_name):
+    global A
+    input = open(file_name, 'w')
+    input.close()
+    for i in range(len(A)):
+        input = open(file_name, 'a')
+        print(*A[i], file=input)
+        input.close()
+    return A
 
-    Параметры:
 
-    **output_filename** — имя входного файла
-    **space_objects** — список объектов планет и звёзд
-    """
-    pass
-
-
-f = open("file.txt",'r')
-file = f.readlines()
-result = file[0].split()[1]
-objects = read_space_objects_data_from_file(file)
-
-for obj in objects:
-    print(obj.mass)
+if __name__ == "__main__":
+    print("This module is not for direct call!")
