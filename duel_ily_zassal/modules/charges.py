1b105deeb1ad8cd8da7f3f6702441d2f163ace06
Art-1313 <gromov.ak@phystech.edu>
2020-12-03 16:50:11 +0300

    charges are ready

--- duel_ily_zassal/modules/charges.py
+++ duel_ily_zassal/modules/charges.py
@@ -1,6 +1,27 @@
-class Electron():
-    pass
+import pygame as pg
+from duel_ily_zassal.modules import vectors
 
+pg.init()
 
-class Monopole():
-    pass
+class Charge():
+
+    def __init__(self, m_c, e_c, screen, coord, color):
+        self.size = 10
+        self.m_c = m_c
+        self.e_c = e_c
+        self.vel = vectors.Vector(0, 50, 0)
+        self.accel = vectors.Vector(0, 0, 0)
+        self.mass = 10
+        self.coord = vectors.Vector(coord[0], coord[1], coord[2])
+        self.damage = 1
+        self.color = color
+        self.screen = screen
+
+    def create(self):
+        pg.draw.circle(self.screen, self.color, (int(self.coord.x), int(self.coord.z)), int(self.size))
+
+    def move(self, dt):
+        self.coord += self.vel * dt
+        self.vel += self.accel * dt
+        self.size = 1000 / self.coord.y
+
--- duel_ily_zassal/modules/manager.py
+++ duel_ily_zassal/modules/manager.py
@@ -1,4 +1,4 @@
-from duel_ily_zassal.modules import gui
+from duel_ily_zassal.modules import gui, charges
 import pygame as pg
 import easygui
 
@@ -8,12 +8,21 @@ class Manager():
     def __init__(self, screen):
         self.screen = screen
         self.done = False
-        self.quit_button = gui.Button('quit', (350,275), self.screen, (100, 50), (375, 285))
+        self.charges = []
+        self.quit_button = gui.Button('quit', (350, 275), self.screen, (100, 50), (375, 285))
+        self.fire_button = gui.Button('fire', (500, 500), self.screen, (100, 50), (375, 285))
 
     def process(self, events):
 
         self.quit_button.create()
         self.quit_button.active()
+
+        self.fire_button.create()
+        self.fire_button.active()
+
+        for charge in self.charges:
+            charge.create()
+            charge.move(0.01)
 
         done = self.event_handler(events)
 
@@ -27,7 +36,12 @@ class Manager():
             if self.quit_button.activated:
                 self.quit_button.click(events, self.quit_b)
                 done = self.done
+            if self.fire_button.activated:
+                self.fire_button.click(events, self.add_charge)
         return done
 
     def quit_b(self):
         self.done = True
+
+    def add_charge(self):
+        self.charges.append(charges.Charge(1, 1, self.screen, (200, 10, 200), (255, 255, 255)))
