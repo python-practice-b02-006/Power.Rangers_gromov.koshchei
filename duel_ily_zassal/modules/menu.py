d8dd9c624793b94e1116b6529ca6e6d75ec11614
Art-1313 <gromov.ak@phystech.edu>
2020-12-15 23:56:28 +0300

    add docstrings

--- duel_ily_zassal/modules/menu.py
+++ duel_ily_zassal/modules/menu.py
@@ -10,8 +10,10 @@ import os
 from modules import gui
 
 
-
 class Menu():
+    '''
+    Menu with some buttons
+    '''
     def __init__(self, scr, screensize):
         self.play_button = gui.Button("Play", (int((screensize[0] - 100)/2), int((screensize[1] - 50)/3)),
                                       scr, (100, 50), (int((screensize[0] - 100)/2+30), int((screensize[1] - 50)/3 +17)))
@@ -22,6 +24,14 @@ class Menu():
                                       (int((screensize[0] - 100) / 2 + 27), int((screensize[1] - 50) / 2 + 17)))
 
     def set_menu(self, scr, screensize):
+        '''
+        Show menu on screen
+        Args:
+            scr: screen
+            screensize: width and high of the screen
+        Returns:
+            None
+        '''
         SC_IMG = pg.image.load(os.path.join("Images", "menu.jpg"))
         new_image = pg.transform.scale(SC_IMG, (screensize))
         scr.blit(new_image, (0, 0))
--- duel_ily_zassal/modules/vectors.py
+++ duel_ily_zassal/modules/vectors.py
@@ -1,25 +1,55 @@
 class Vector:
+    '''
+    Provide vector calculation
+    '''
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
     def __add__(self, other):
+        '''
+        Sum of to Vectors
+        Args:
+            other: other Vector
+        Returns:
+            Vector
+        '''
         return Vector(self.x + other.x,
                       self.y + other.y,
                       self.z + other.z)
 
     def __sub__(self, other):
+        '''
+        Sub of to Vectors
+        Args:
+            other: other Vector
+        Returns:
+            Vector
+        '''
         return Vector(self.x - other.x,
                       self.y - other.y,
                       self.z - other.z)
 
     def __matmul__(self, other):
+        '''
+        Vector multiplication of two vectors
+        Args:
+            other: other vector
+
+        Returns:
+            Vector
+        '''
         return Vector(self.y * other.z - self.z * other.y,
                       self.z * other.x - self.x * other.z,
                       self.x * other.y - self.y * other.x)
 
     def __abs__(self):
+        '''
+        Vector module
+        Returns:
+            float
+        '''
         return (self.x**2 + self.y**2 + self.z**2)**0.5
 
     def __mul__(self, other):
@@ -36,6 +66,7 @@ class Vector:
 
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")"
+
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
